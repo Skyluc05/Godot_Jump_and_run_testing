9a1e944975aa9ccc125b513f1a1158604395088d
Skyluc05 <152433467+Skyluc05@users.noreply.github.com>
2024-11-17 00:14:36 +0100

    DEATH TRIGGER

---
+++ node_2d.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	print("test")# Replace with function body.
